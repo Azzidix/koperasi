e8201f051ca1f0617f9b176c8f7faa6dc7c755e2
Husein Azzidix <huseinazidix12@gmail.com>
2019-05-06 11:35:31 +0700

    perubahan beberapa source code dan menampilkan  data anggota

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,6 +8,7 @@ class Dashboard extends CI_Controller
 	{
 		parent::__construct();	
 		$this->load->library('session');
+		$this->load->model('m_login','mdata');
 	}
 
 	public function index()
@@ -22,12 +23,32 @@ class Dashboard extends CI_Controller
 
 	public function data_anggota()
 	{
-		$this->load->view('admin/pages/data-anggota');
+		$data['anggota'] = $this->mdata->tampil_data_anggota();
+		$this->load->view('admin/pages/data-anggota',$data);
 	}
 
 	public function tambah_anggota()
 	{
-		$this->load->view('admin/pages/tambah-anggota');	
+		$this->load->view('admin/pages/tambah-anggota');
+		$tambah = $this->input->post('simpan');
+		if (isset($tambah))	
+		{
+			$data = array(
+					'nama' => $this->input->post('nama'),
+					'alamat' => $this->input->post('alamat'),
+					'no_telpon' => $this->input->post('no_telpon'),
+					'jenis_kelamin' => $this->input->post('jenis_kelamin'),
+					'tanggal_lahir' => $this->input->post('tanggal_lahir'),
+					'tanggal_gabung' => date('y/m/d'),
+					'level' => $this->input->post('level'),
+					'status' => 0,
+				);
+			if ($this->mdata->tambah_anggota($data) == true) {
+				$this->session->set_flashdata('message', 'berhasil');
+			} else {
+				$this->session->set_flashdata('message', 'gagal');
+			}
+		}
 	}
 
 	public function data_simpanan()

d1516727d5462844afd8d4a32805f61cf6e30dc1
Husein Azzidix <huseinazidix12@gmail.com>
2019-05-06 11:30:14 +0700

    penambahan function tampil data anggota, tambah data anggota, tampil data simpanan, tambah data simpanan, tampil data pinjaman, tambah data simpanan, dan perbaikan beberapa query database

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -9,14 +9,79 @@ class m_login extends CI_Model
 
 	public function login_validasi($data)
 	{
-		$this->db->where('username', $data['username']);
-		$this->db->where('password', $data['password']);
+		$this->db->where('username', $data['user']);
+		$this->db->where('password', $data['pass']);
 
 		$sql = $this->db->get('anggota');
 
 		if ($this->db->affected_rows() == 1) {
+			return $sql->result();
+		} else {
+			return false;
+		}
+	}
+
+	public function tampil_data_anggota()
+	{
+		$this->db->order_by('level');
+		$sql = $this->db->get('anggota');
+
+		if ($this->db->affected_rows() >= 0) {
+			return $sql->result();
+		} else {
+			return false;
+		}
+	}
+
+	public function tambah_anggota($data)
+	{
+		$sql = $this->db->insert('anggota', $data);
+		if ($this->db->affected_rows() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function tampil_data_simpanan()
+	{
+		$sql = $this->db->get('simpanan');
+
+		if ($this->db->affected_rows() > 0) {
+			return $sql->result();
+		} else {
+			return false;
+		}
+	}
+	public function tambah_simpanan($data)
+	{
+		$sql = $this->db->insert('simpanan', $data);
+		if ($sql) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function tampil_data_pinjaman()
+	{
+		$sql = $this->db->get('pinjaman');
+
+		if ($this->db->affected_rows() > 0) {
 			# code...
 			return $sql->result();
+		} else {
+			return false;
+		}
+	}
+
+	public function tambah_pinjaman($data)
+	{
+		$sql = $this->db->insert('pinjaman', $data);
+		if ($sql) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
